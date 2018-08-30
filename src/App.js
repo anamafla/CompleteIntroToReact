fe8cfdab7e18290b17822dfe6c73a49db19b0f65
Ana Mafla <anamafla@hotmail.com>
2018-08-29 20:43:59 -0500

    State and Lifecycle, Reach Router

--- src/App.js
+++ src/App.js
@@ -1,69 +1,23 @@
 import React from "react";
-import { render } from "react-dom";
-import Pet from "./Pet";
-import pf from "petfinder-client";
-
-const petfinder = pf ({
-  key: process.env.API_KEY,
-  secret: process.env.API_SECRET
-});
-
-
+import ReactDOM from "react-dom";
+import Results from "./Results";
+import { Router, Link } from "@reach/router";
+import Details from "./Details";
 
 class App extends React.Component {
-
-  constructor(props) {
-    super(props);
-
-    this.state = {
-      pets: []
-    }
-  }
-  componentDidMount() {
-    petfinder.pet.find ({ output: "full", location: "Seattle, WA"})
-    .then(data => {
-      let pets;
-      if (data.petfinder.pets && data.petfinder.pets.pet) {
-        if(Array.isArray(data.petfinder.pets.pet)){
-          pets = data.petfinder.pets.pet;
-        } else {
-          pets = [data.petfinder.pets.pet];
-        }
-      } else {
-        pets = []
-      }
-      this.setState({
-        pets
-      });
-    });
-  }
   render() {
     return (
       <div>
-      <h1>Adopt Me!</h1>
-      <div>
-        {this.state.pets.map( pet => {
-          let breed;
-          if (Array.isArray(pet.breeds.bredd)){
-            breed = pet.breeds.breed.join(", ");
-          } else {
-            breed = pet.breeds.breed;
-          }
-          return (
-            <Pet
-              key={pet.id}
-              animal={pet.animal}
-              name={pet.name}
-              breed={breed}
-            />
-          )
-        }
-        )}
-        </div>
+        <header>
+          <Link to="/"> Adopt Me! </Link>
+        </header>
+        <Router>
+          <Results path="/" />
+          <Details path="/details/:id" />
+        </Router>
       </div>
-
     );
   }
 }
 
-render(<App/>, document.getElementById("root"));
+ReactDOM.render(React.createElement(App), document.getElementById("root"));
